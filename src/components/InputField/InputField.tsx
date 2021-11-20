c13182c718ffa821633f2766d6dc3459642057a0
Anna Chen <chenxanna@gmail.com>
2021-11-20 15:05:45 +0100

    Add InputField Component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./InputField.css";
 
-interface InputFieldProps {}
+interface InputFieldProps {
+  label: string;
+  validation: string;
+  placeholder: string;
+  pattern: string;
+}
 
-export const InputField = ({ ...props }: InputFieldProps) => {
-  // --- YOUR CODE HERE ---
-
-  return <>InputField</>;
+export const InputField = ({
+  label,
+  validation,
+  placeholder,
+  pattern,
+}: InputFieldProps) => {
+  return (
+    <div>
+      <div className="input-field-label">{label}</div>
+      <input
+        className="input-field-input"
+        placeholder={placeholder}
+        pattern={pattern}
+      />
+      <div className="input-field-invalid-text">
+        {validation}
+      </div>
+    </div>
+  );
 };
